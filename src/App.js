1b012879b1c40d9d1100938628d1506f504a90af
Noe <eon.sberk@gmail.com>
2021-09-05 18:19:25 +0200

    rename files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.scss';
+import Navbar from './components/Navbar/Navbar'
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hallo</h1>
     </div>
   );
