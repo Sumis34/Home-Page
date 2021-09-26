8998a925128ac605bd7d72787fa75df8431b9a63
Noe <eon.sberk@gmail.com>
2021-09-26 12:07:56 +0200

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './footer.scss';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <footer>
+                
+            </footer>
+        );
+    }
+}
