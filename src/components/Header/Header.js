91d7737c18e57d5fca04d0fb0599ca8401927589
Noe <eon.sberk@gmail.com>
2021-09-29 22:15:15 +0200

    testing with video

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './header.scss';
-import blob from '../../assets/images/blob.png'
-
+// import blob from '../../assets/images/blob.png'
+import bg from '../../assets/videos/FebruarWeb-1.m4v'
 export default class Header extends Component {
     render() {
         return (
@@ -11,10 +11,13 @@ export default class Header extends Component {
                         <h1 className="title">Noé Krebs</h1>
                         <h2 className="sub-title">Development and design</h2>
                     </div>
-                    <div className="col col-4">
+                    {/* <div className="col col-4">
                         <img className="blob" src={blob} alt="himmel"/>
-                    </div>
+                    </div> */}
                 </div>
+                <video src={bg} autoPlay muted loop poster="posterimage.jpg">
+                        Sorry, your browser doesn't support embedded videos
+                </video>
             </header>
         );
     }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './home.scss';
-import Header  from '../../components/Header/Header.js'
-import Waves from '../../assets/design-elements/waves-5.svg'
+import Header from '../../components/Header/Header.js';
+import Waves from '../../assets/design-elements/waves-5.svg';
+// import bg from '../../assets/videos/FebruarWeb-1.m4v'
 //import Discord from '../../components/Discord/Discord.js'
 
 
