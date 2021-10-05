905a286a8d86c29a11d214f191093d327a736a85
Noe <Noe>
2021-10-05 18:44:20 +0200

    implementing mobile nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,22 +7,42 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/"><img className="logo" src={logo} alt="logo" /></Link>
-                        </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <div className="nav-desktop">
+                    <nav>
+                        <div className="nav-icons">
+                            <Link to="/"><img className="logo" src={logo} alt="logo" /></Link><i className="fas fa-bars burger"></i>
+                        </div>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="nav-mobile">
+                    <div className="nav-icons">
+                        <Link to="/"><img className="logo" src={logo} alt="logo" /></Link><i className="fas fa-bars burger"></i>
+                    </div>
+                    <nav className="mobile">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         );
     }
