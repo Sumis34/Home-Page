30d499e400de49fa5bd068d9e02904261b672e14
Noe <Noe>
2021-10-06 15:57:37 +0200

    first section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './home.scss';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
+import Work from './sections/work'
 // import Waves from '../../assets/design-elements/waves-5.svg';
 // import bg from '../../assets/videos/FebruarWeb-1.m4v'
 //import Discord from '../../components/Discord/Discord.js'
@@ -12,6 +13,7 @@ export default function Home() {
         <>
             <Header />
             {/* <img src={Waves} alt="Waves" /> */}
+            <Work />
             <div className="home"></div>
             <Footer />
         </>
---
+++ src/pages/Home/sections/work.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../home.scss';
+
+
+export default function Work() {
+    return (
+        <>
+            <section>
+                <p>Work</p>
+            </section>
+        </>
+    )
+}
