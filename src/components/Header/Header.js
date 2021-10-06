ba827eff0b26f360eb8ca4d4f18b94f1fffa175b
Noe <Noe>
2021-10-06 16:04:44 +0200

    test button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,7 @@ export default function Header() {
                     <div className="col">
                         <h1 className="title">Noé Krebs</h1>
                         <h2 className="sub-title">Development and design</h2>
+                        {/* <button type="button" className="btn btn-primary">Hallo</button> */}
                     </div>
                 </div>
                 {/* <p>47° 1' 41.3544" 7° 24' 47.5194"</p> */}
