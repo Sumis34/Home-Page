32ae30fcb1867825d23b469452c067a2998e7d83
Noe <eon.sberk@gmail.com>
2021-10-18 22:33:40 +0200

    card style

--- src/components/Cards/WorkCard.js
+++ src/components/Cards/WorkCard.js
@@ -8,15 +8,15 @@ export default function WorkCard() {
         <>
             <div className="place-holder"></div>
             <div className="work-card-body">
-                <img src={img} alt="Logo" />
+                
                 <div className="work-card-header">
                     <h2 className="work-card-title">Engadin</h2>
                     <h3 className="work-card-description">Persönliches Projekt</h3>
                 </div>
-
                 <div className="work-card-wrap">
                     <a href="" className="work-card-link">Video auf Youtube schauen</a>
                 </div>
+                <img src={img} alt="Logo" />
             </div>
 
         </>
