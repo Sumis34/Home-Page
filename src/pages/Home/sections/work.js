93cf4e86cf545b767cf8dc2a40828bfdc08d308d
Noe <eon.sberk@gmail.com>
2021-10-22 22:29:10 +0200

    drag indicator

--- src/pages/Home/sections/work.js
+++ src/pages/Home/sections/work.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../home.scss';
 import ScrollContainer from 'react-indiana-drag-scroll'
 import Card from '../../../components/Cards/WorkCard'
 import img from '../../../assets/images/engadin.png'
+import swipe from '../../../assets/icons/swipe.png'
 
 export default function Work() {
-    return (
 
+    //const [draged, setDraged] = useState(false);
+
+    useEffect(() => {
+        const cards = document.getElementById("work-cards");
+        cards.addEventListener("mousedown", () =>{
+            const swipe = document.getElementById("work-swipe");
+            console.log("Darging")
+            swipe.classList.remove("animation");
+        });
+    });
+
+    return (
         <section id="work">
             <p>Work</p>
+            <img src={swipe} alt="swipe" className="swipe animation" id="work-swipe"/>
             <div className="fade-right"></div>
             <ScrollContainer className="scroll-container indiana-scroll-container indiana-scroll-container--hide-scrollbars" horizontal={true}>
-                <div className="cards">
+                <div className="cards" id="work-cards">
                     <Card
                         title="Engadin"
                         description="Persönliches Projekt"
