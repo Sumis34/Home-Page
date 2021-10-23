5f1722995b0060180013f51aca515061fdd3c6df
Noe <eon.sberk@gmail.com>
2021-10-23 09:58:06 +0200

    change work layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import './home.scss';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
 import Work from './sections/work'
+import Contact from './sections/contact'
 // import Waves from '../../assets/design-elements/waves-5.svg';
 // import bg from '../../assets/videos/FebruarWeb-1.m4v'
 //import Discord from '../../components/Discord/Discord.js'
@@ -14,6 +15,7 @@ export default function Home() {
             <Header />
             {/* <img src={Waves} alt="Waves" /> */}
             <Work />
+            <Contact />
             <div className="home" id="home"></div>
             <Footer />
         </>
--- src/pages/Home/sections/work.js
+++ src/pages/Home/sections/work.js
@@ -18,64 +18,64 @@ export default function Work() {
 
     useEffect(() => {
         const cards = document.getElementById("work-cards");
-        cards.addEventListener("mousedown", () =>{
+        cards.addEventListener("mousedown", () => {
             const swipe = document.getElementById("work-swipe");
             swipe.classList.remove("animation");
         });
     });
 
     return (
-        <section id="work">
-            <Line />
-            <h2>My work.</h2>
-            <img src={blob} alt="..." id="work-blob"/>
-            <img src={swipe} alt="swipe" className="swipe animation" id="work-swipe"/>
-            <img src={arrow} alt="arrow" className="arrow" id="work-arrow"/>
-            
-            <div className="fade-right"></div>
-            <ScrollContainer className="scroll-container indiana-scroll-container indiana-scroll-container--hide-scrollbars" horizontal={true}>
-                <div className="cards" id="work-cards">
-                    <Card
-                        title="Engadin"
-                        description="Persönliches Projekt"
-                        link="http://google.ch"
-                        linkTitel="Video auf Youtube schauen"
-                        img={img}
-                    />
-                    <Card
-                        title="UHCG"
-                        description="Ein Auftrag vom UHCG"
-                        link="http://google.ch"
-                        linkTitel="Video auf Youtube schauen"
-                        img={uhcg}
-                    />
-                    <Card
-                        title="Chasing winter"
-                        description="Persönliches Projekt"
-                        link="http://google.ch"
-                        linkTitel="Video auf Youtube schauen"
-                        img={ski}
-                    />
-                    <Card
-                        title="The beauty of FPV"
-                        description="Persönliches Projekt"
-                        link="http://google.ch"
-                        linkTitel="Video auf Youtube schauen"
-                        img={fpv}
-                        font="dark"
-                    />
-                    <Card
-                        title="Zermatt"
-                        description="Persönliches Projekt"
-                        link="http://google.ch"
-                        linkTitel="Video auf Youtube schauen"
-                        img={zermatt}
-                    />
+        <>
+            <section id="work">
+                <div className="fade-right"></div>
+                <div id="work-content">
+                    <Line />
+                    <h2>My work.</h2>
+                    <img src={blob} alt="..." id="work-blob" />
+                    <img src={swipe} alt="swipe" className="swipe animation" id="work-swipe" />
+                    <img src={arrow} alt="arrow" className="arrow" id="work-arrow" />
+                    <ScrollContainer className="scroll-container indiana-scroll-container indiana-scroll-container--hide-scrollbars" horizontal={true}>
+                        <div className="cards" id="work-cards">
+                            <Card
+                                title="Engadin"
+                                description="Persönliches Projekt"
+                                link="http://google.ch"
+                                linkTitel="Video auf Youtube schauen"
+                                img={img}
+                            />
+                            <Card
+                                title="UHCG"
+                                description="Ein Auftrag vom UHCG"
+                                link="http://google.ch"
+                                linkTitel="Video auf Youtube schauen"
+                                img={uhcg}
+                            />
+                            <Card
+                                title="Chasing winter"
+                                description="Persönliches Projekt"
+                                link="http://google.ch"
+                                linkTitel="Video auf Youtube schauen"
+                                img={ski}
+                            />
+                            <Card
+                                title="The beauty of FPV"
+                                description="Persönliches Projekt"
+                                link="http://google.ch"
+                                linkTitel="Video auf Youtube schauen"
+                                img={fpv}
+                                font="dark"
+                            />
+                            <Card
+                                title="Zermatt"
+                                description="Persönliches Projekt"
+                                link="http://google.ch"
+                                linkTitel="Video auf Youtube schauen"
+                                img={zermatt}
+                            />
+                        </div>
+                    </ScrollContainer>
                 </div>
-
-            </ScrollContainer>
-
-        </section>
-
+            </section>
+        </>
     )
 }
