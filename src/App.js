1413c63d62b89fbe78375501b386bef1bf4a066a
Noe <eon.sberk@gmail.com>
2021-10-24 19:47:09 +0200

    contact waves and working favicon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.scss';
 import React, { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Router from './components/Router/Router'
+import favBright from './assets/images/nk-logo_bright.png'
+import favDark from './assets/images/nk-logo_dark.png'
 
 
 function App() {
@@ -12,12 +14,12 @@ function App() {
       const favicon = document.getElementById("favicon");
       //Check browser theme
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        favicon.href = "./assets/images/nk-logo_bright.png";
+        favicon.href = favBright;
         console.log("Dark");
       }
 
       else {
-        favicon.href = "./assets/images/nk-logo_dark.png";
+        favicon.href = favDark;
         console.log("Light");
       }
     };
--- src/pages/Home/sections/contact.js
+++ src/pages/Home/sections/contact.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import waveTop from '../../../assets/design-elements/wave-1.svg'
+import waveBottom from '../../../assets/design-elements/wave-2.svg'
+import phone from '../../../assets/design-elements/phone-alt-solid.svg'
+import email from '../../../assets/design-elements/envelope-solid.svg'
+import './contact.scss'
 
 export default function contact() {
     return (
         <div>
-            hallo
+            <section id="contact">
+                <img src={waveTop} alt="wave-top" id="wave-top" />
+                <div className="contact-bg">
+                    <div className="contact-cont">
+                        <div className="contact-header">
+                            <h2>Hast du eine Vision?</h2>
+                            <p>In Zusammenarbeit erstellen wir einen
+                                einzigartigen Film der zu dir passt. </p>
+                        </div>
+                        <div className="contact-info">
+                            <div className="phone">
+                                <img src={phone} alt="phone" className="contact-icon" />
+                                <h3>Telephone</h3>
+                                <a href="#">Hallo</a>
+                            </div>
+                            <div className="email">
+                                <img src={email} alt="email" className="contact-icon" />
+                                <h3>E-mail</h3>
+                                <a href="#">email</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img src={waveBottom} alt="wave-bottom" id="wave-bottom" />
+            </section>
         </div>
     )
 }
