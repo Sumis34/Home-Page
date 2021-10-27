2fe461c1e3520e81f17f2d38112ec90d68a2bf8d
Noe <eon.sberk@gmail.com>
2021-10-27 20:43:49 +0200

    custom fade in test

--- src/pages/Home/sections/philosophie.js
+++ src/pages/Home/sections/philosophie.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import bg from '../../../assets/images/forest.png'
 import Line from '../../../components/AccentLine/AccentLine'
+import Fade from 'react-reveal/Fade'
+
 
 export default function philosophie() {
     return (
         <div>
             <section id="philosophie">
                 <img src={bg} alt="forest" id="forest" />
-                <div className="content">
-                    <Line />
-                    <h2>Philosophie.</h2>
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quis reprehenderit non ipsam, illum facilis odit. Quibusdam cum consectetur totam veniam obcaecati commodi et repudiandae ut corrupti. Fuga, tempora deleniti.
-                    </p>
-                </div>
+                <Fade bottom >
+                    <div className="content">
+                        <Line />
+                        <h2>Philosophie.</h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quis reprehenderit non ipsam, illum facilis odit. Quibusdam cum consectetur totam veniam obcaecati commodi et repudiandae ut corrupti. Fuga, tempora deleniti.
+                        </p>
+                    </div>
+                </Fade>
+                
             </section>
         </div>
     )
