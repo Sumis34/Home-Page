d9d1463ad46a3d5a4ec4dd9aa010721f85751711
Noe <eon.sberk@gmail.com>
2021-11-12 20:39:26 +0100

    animation on video page

--- src/pages/VideoInfo/VideoInfo.js
+++ src/pages/VideoInfo/VideoInfo.js
@@ -3,8 +3,8 @@ import './VideoInfo.scss'
 import { useParams } from "react-router-dom";
 import VideoData from '../../data/cards.json'
 import { useHistory } from 'react-router-dom';
-import Fade from 'react-reveal/Fade'
 import x from '../../assets/icons/x-mark-thin-light.svg'
+import Fade from 'react-reveal/Fade'
 import Reveal from 'react-reveal/Reveal'
 
 export default function VideoInfo() {
@@ -30,15 +30,15 @@ export default function VideoInfo() {
             <img src={x} alt="x" id="close-contact-menu" className="close-btn" onClick={() => closeForm()} />
 
             <div className="video-content">
-                <Reveal effect="fadeInUp">
+                <Fade bottom cascade>
                     <div className="infos">
                         <h1>{videoInfo.title}</h1>
                         <p>{videoInfo.description}</p>
                         <p className="date">{videoInfo.date}</p>
                     </div>
-                </Reveal>
+                </Fade>
                 <div className="video">
-                    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/b4vF_8mZ320?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src={videoInfo.link} title="YouTube video player" className="yt-video video-reveal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
             <div className="bg-image">
