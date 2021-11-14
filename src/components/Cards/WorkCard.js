8cfa0b4125cb01dbba5ce17cb73f0ac2a0380b9b
Noe <eon.sberk@gmail.com>
2021-11-14 21:19:35 +0100

    removed card wrap

--- src/components/Cards/WorkCard.js
+++ src/components/Cards/WorkCard.js
@@ -8,9 +8,6 @@ export default function WorkCard(props) {
                 <h2 className="work-card-title">{props.title}</h2>
                 <h3 className="work-card-description">{props.type}</h3>
             </div>
-            <div className="work-card-wrap">
-                <a href={props.link} className="work-card-link">{props.linkTitel}</a>
-            </div>
             <img src={require(`../../assets/images/${props.img}`).default} alt="Logo" />
         </div>
     )
