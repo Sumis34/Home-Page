b737fa049ae4eae19e78e95598a30d36d0ae3e40
Noe <eon.sberk@gmail.com>
2021-12-08 18:50:23 +0100

    light wait footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,26 +5,7 @@ import {NavLinks} from '../Navbar/Navbar'
 export default function Footer() {
     return (
         <footer>
-            <div className="top-footer">
-                <div className="company">
-                    <h1>Noé Krebs</h1>
-                    <p>I build your experiance</p>
-                </div>
-                <div className="explore">
-                    <ul>
-                        <NavLinks />
-                    </ul>
-                </div>
-                <div className="quik">
-                    <h2>Quick Links</h2>
-                    <a href="/">FreeTrasfer</a>
-                    <a href="/">Test</a>
-                </div>
-            </div>
-            <div className="social-media"></div>
-            <div className="copyright">
-                <p>Copyright © 2016 - 2021 Noé Krebs</p>
-            </div>
+            <p>Made with ❤️, by Noé Krebs!</p>
         </footer>
     );
 }
--- src/pages/Home/sections/about.js
+++ src/pages/Home/sections/about.js
@@ -11,6 +11,7 @@ export default function About() {
     useEffect(() => {
         window.onscroll = () => {
             setOffset(window.pageYOffset)
+            
         }
     }, []);
 
